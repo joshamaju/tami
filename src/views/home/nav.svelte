a5bb19afc4033dc7c2149428c0276cf670eec30d
Joshua Amaju <joshsemail4work@gmail.com>
2025-02-14 22:56:20 +0100

    chore: add empty requests list state

--- src/views/home/nav.svelte
+++ src/views/home/nav.svelte
@@ -2,6 +2,7 @@
   import PlusIcon from "lucide-svelte/icons/plus";
   // import MenuIcon from "lucide-svelte/icons/menu";
   import CopyIcon from "lucide-svelte/icons/copy";
+  import GlobeIcon from "lucide-svelte/icons/globe";
   import TrashIcon from "lucide-svelte/icons/trash-2";
   //   import EllipsisIcon from "lucide-svelte/icons/ellipsis-vertical";
 
@@ -19,7 +20,9 @@
 
   const current_session = session;
 
-  const get_url = (url: string) => {
+  const get_url = (url: string | null | undefined) => {
+    if (!url) return null;
+
     try {
       return new URL(url);
     } catch (error) {
@@ -35,81 +38,84 @@
     $$restProps.class ?? '',
   ].join(' ')}"
 >
-  <div class="flex justify-between px-4">
-    <!-- <button class="js-toggle-side-nav">
-      <MenuIcon size="{20}" class="pointer-events-none" />
-    </button> -->
-
-    <div></div>
-
-    <form action="/session/new" method="post">
-      <button>
-        <PlusIcon size="{20}" />
-      </button>
-    </form>
-  </div>
-
-  <ul class="space-y-2 text-sm">
-    {#each sessions as session}
-      {@const request = session.request}
-      {@const url = get_url(request.url)}
-      <li
-        title="{request ? `${request.method} ${request?.url}` : null}"
-        class="flex items-center justify-between px-4 py-1 gap-2 rounded-md {current_session.slug ==
-        session.slug
-          ? 'bg-slate-300'
-          : ''}"
-      >
-        <a
-          href="/?session={session.slug}"
-          class="truncate whitespace-nowrap space-x-2"
+  {#if sessions.length <= 0}
+    <div class="h-full flex flex-col items-center justify-center gap-3">
+      <div class="p-4 rounded-full bg-slate-200">
+        <GlobeIcon size="{100}" class="text-gray-300" />
+      </div>
+      <h1 class="text-gray-500">Nothing here yet!</h1>
+      <form action="/session/new" method="post" class="flex">
+        <button
+          class="bg-[#4660ff] rounded-md py-1 px-4 text-white text-sm flex items-center gap-1 shadow-md"
         >
-          {#if url}
-            <span
-              class="font-medium {request.method == Method.GET
-                ? 'text-green-600'
-                : request.method == Method.POST
-                  ? 'text-blue-600'
-                  : request.method == Method.PUT
-                    ? 'text-orange-600'
-                    : request.method == Method.DELETE
-                      ? 'text-red-600'
-                      : ''}"
-            >
-              {request.method}
-            </span>
-            <span>{url.hostname}{url.pathname}</span>
-          {:else}
-            <span>{session.slug}</span>
-          {/if}
-        </a>
+          <PlusIcon size="{15}" />
+          <span>New request</span>
+        </button>
+      </form>
+    </div>
+  {:else}
+    <ul class="space-y-2 text-sm">
+      {#each sessions as session}
+        {@const request = session.request}
+        {@const url = get_url(request?.url)}
+        <li
+          title="{request ? `${request.method} ${request?.url}` : null}"
+          class="flex items-center justify-between px-4 py-1 gap-2 rounded-md {current_session.slug ==
+          session.slug
+            ? 'bg-slate-300'
+            : ''}"
+        >
+          <a
+            href="/?session={session.slug}"
+            class="truncate whitespace-nowrap space-x-2"
+          >
+            {#if request && url}
+              <span
+                class="font-medium {request.method == Method.GET
+                  ? 'text-green-600'
+                  : request.method == Method.POST
+                    ? 'text-blue-600'
+                    : request.method == Method.PUT
+                      ? 'text-orange-600'
+                      : request.method == Method.DELETE
+                        ? 'text-red-600'
+                        : ''}"
+              >
+                {request.method}
+              </span>
+              <span>{url.hostname}{url.pathname}</span>
+            {:else}
+              <span>{session.slug}</span>
+            {/if}
+          </a>
 
-        <div class="hidden actions">
-          <div class="flex gap-2 items-center">
-            <!-- <button type="button" popovertarget="popover-action-{i}">
+          <div class="hidden actions">
+            <div class="flex gap-2 items-center">
+              <!-- <button type="button" popovertarget="popover-action-{i}">
               <EllipsisIcon size="{15}" />
             </button> -->
 
-            <form method="post" action="/session/duplicate">
-              <button name="slug" value="{session.slug}">
-                <CopyIcon size="{15}" />
-              </button>
-            </form>
+              <form method="post" action="/session/duplicate">
+                <button name="slug" value="{session.slug}">
+                  <CopyIcon size="{15}" />
+                </button>
+              </form>
 
-            <form action="/session/delete" method="post" class="contents">
-              <button name="slug" value="{session.slug}">
-                <TrashIcon size="{15}" />
-              </button>
-            </form>
-          </div>
+              <form action="/session/delete" method="post" class="contents">
+                <button name="slug" value="{session.slug}">
+                  <TrashIcon size="{15}" />
+                </button>
+              </form>
+            </div>
 
-          <!-- <div popover="auto" id="popover-action-{i}">
+            <!-- <div popover="auto" id="popover-action-{i}">
             <p>popover</p>
           </div> -->
-        </div>
-      </li>
-    {/each}
-  </ul>
+          </div>
+        </li>
+      {/each}
+    </ul>
+  {/if}
 </nav>
 
 <style>
