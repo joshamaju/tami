fa4fc637f18fb8e846ba3f37817bb332219928df
Joshua Amaju <joshsemail4work@gmail.com>
2025-02-19 17:54:27 +0100

    fix: use fully resolved static directory path
    This is an issue because of issues locating the static directory when executed using something like npx

---
+++ .changeset/hip-turtles-try.md
@@ -0,0 +1,5 @@
+---
+"tami-client": patch
+---
+
+Use fully resolved static directory for serving static assets
--- bin/server.js
+++ bin/server.js
@@ -6,6 +6,7 @@ import router from "../dist/server/index.js";
 const app = express();
 
 const build_path = fileURLToPath(new URL("../dist", import.meta.url));
+const static_path = fileURLToPath(new URL("../static", import.meta.url));
 
 const serve_build_assets = static_(build_path, {
   immutable: true,
@@ -16,7 +17,7 @@ app.disable("x-powered-by");
 
 app.use(serve_build_assets);
 
-app.use(static_("static", { maxAge: "1h" }));
+app.use(static_(static_path, { maxAge: "1h" }));
 
 app.use(router);
 
